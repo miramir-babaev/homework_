202f9f44ba8be5d8bdac4f1aa77001b082214851
Miramir Babaev <miramir.babaev@yandex.ru>
2023-07-23 15:44:54 +0300

    github

---
+++ hw1/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 1: Задайте значение N. Напишите программу, которая выведет все ЧЕТНЫЕ натуральные числа в промежутке от 1 до N. Выполнить с помощью рекурсии.
+N = 5, M = 9-> "6, 8"
+N = 2, M = 10-> "2, 4, 6, 8, 10" */
+
+Console.Write("Введите первое число => ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число => ");
+int n = Convert.ToInt32(Console.ReadLine());
+void EvenNum(int m, int n)
+{
+    if (m > n)
+        return;
+    if (m % 2 == 0)
+    {
+        Console.Write($"{m},");
+    }
+    EvenNum(m + 1, n);
+}
+EvenNum(m, n);
---
+++ hw2/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+Console.Write("Введите первое число => ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число => ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Num(m, n);
+
+void Num(int m, int n)
+{
+    Console.WriteLine(SumNum(m - 1, n));
+}
+
+int SumNum(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumNum(m, n);
+        return res;
+    }
+}
